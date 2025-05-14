f03774aa75f8df8ef56b1453dc8197cdacccf6f3
totopark0 <48053210+ParkKyuIl@users.noreply.github.com>
2025-05-15 01:38:55 +0900

    Time: 6 ms (31.77%), Space: 44.4 MB (26.39%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String val = String.valueOf(x);
+        int left = 0, right = val.length()-1;
+
+        
+        while(left < right){
+            if(val.charAt(left) != val.charAt(right)) return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}

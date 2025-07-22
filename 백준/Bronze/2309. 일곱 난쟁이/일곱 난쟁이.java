2385a9257b89072b082645cf8e22f414d19f8bab
totopark0 <48053210+ParkKyuIl@users.noreply.github.com>
2025-07-23 00:43:52 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -2,46 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static int[] heights = new int[9];
+    static boolean[] visited = new boolean[9];
+    static int[] selected = new int[7];
+    static boolean found = false;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] heights = new int[9];
-        int sum = 0;
 
         for (int i = 0; i < 9; i++) {
             heights[i] = Integer.parseInt(br.readLine());
-            sum += heights[i];
         }
 
-        int fakeSum = sum - 100;
-        int fake1 = 0;
-        int fake2 = 0;
-
-        for(int i=0;i<9;i++){
-            for(int j=i+1;j<9;j++){
-                if(heights[j]+heights[i] == fakeSum){
-                    fake1 = heights[i];
-                    fake2 = heights[j];
-                }
-            }
-        }
-
-        List<Integer> result = new ArrayList<>();
-
-        for(int i=0;i<9;i++){
-            if(heights[i] == fake1 || heights[i] ==  fake2){
-                continue;
-            }
-            result.add(heights[i]);
-        }
-
-        Collections.sort(result);
-
-        StringBuilder sb = new StringBuilder();
-
-        for(int h: result) sb.append(h).append("\n");
-
-        System.out.println(sb);
+        permute(0,0);
 
 
     }
+
+    static void permute(int depth, int start){
+
+        if(found) return;
+        int sum = 0;
+        if(depth == 7) {
+            for (int i = 0; i < 7; i++) {
+                sum += selected[i];
+            }
+            if (sum == 100) {
+                Arrays.sort(selected);
+                for (int h : selected) {
+                    System.out.println(h);
+                }
+                found = true;
+            }
+            return;
+        }
+
+
+
+        for (int i = start; i < 9; i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                selected[depth] = heights[i];
+                permute(depth+1,i+1);
+                visited[i] = false;
+            }
+
+        }
+
+        }
 }
